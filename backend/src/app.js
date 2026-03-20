f1fe52c56019a7f82c334d650265c601add2d971
KshitizChingawat <147234964+KshitizChingawat@users.noreply.github.com>
2026-03-20 15:04:28 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,36 @@
+// import express from "express";
+// import cors from "cors";
+
+// // import authRoutes from "./routes/auth.routes.js";
+// import vendorRoutes from "./routes/vendor.routes.js";
+// import menuRoutes from "./routes/menu.routes.js";
+// import orderRoutes from "./routes/order.routes.js";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // app.use("/api/auth", authRoutes);
+// app.use("/api/vendors", vendorRoutes);
+// app.use("/api/menus", menuRoutes);
+// app.use("/api/orders", orderRoutes);
+
+// export default app;
 import express from "express";
 import cors from "cors";
-
-// import authRoutes from "./routes/auth.routes.js";
-import vendorRoutes from "./routes/vendor.routes.js";
-import menuRoutes from "./routes/menu.routes.js";
-import orderRoutes from "./routes/order.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/auth", authRoutes);
-app.use("/api/vendors", vendorRoutes);
-app.use("/api/menus", menuRoutes);
-app.use("/api/orders", orderRoutes);
+app.get("/api/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Veg Burger", price: 80, category: "snacks", veg: true },
+    { id: 2, name: "Pizza", price: 120, category: "meal", veg: true },
+    { id: 3, name: "Chicken Roll", price: 150, category: "meal", veg: false }
+  ]);
+});
 
 export default app;
