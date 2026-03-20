6bb75458f1f8cfdfb8bc2bb1246b882f37d76624
KshitizChingawat <147234964+KshitizChingawat@users.noreply.github.com>
2026-03-20 14:54:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.routes.js";
+// import authRoutes from "./routes/auth.routes.js";
 import vendorRoutes from "./routes/vendor.routes.js";
 import menuRoutes from "./routes/menu.routes.js";
 import orderRoutes from "./routes/order.routes.js";
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+// app.use("/api/auth", authRoutes);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/menus", menuRoutes);
 app.use("/api/orders", orderRoutes);
